47d4c6d306abebc7de0947b8bf2dc59a8d784783
ChristopherLerum <146862377+ChristopherLerum@users.noreply.github.com>
2023-11-07 09:45:08 -0800

    test commit

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 rm -rf grading-area
@@ -7,6 +7,34 @@ mkdir grading-area
 
 git clone $1 student-submission
 echo 'Finished cloning'
+
+files=`find student-submission/*.java`
+
+if [[ ! -e $files ]]
+then
+    echo 'Submission is not a file'
+    exit
+fi
+
+echo 'test'
+
+cp $files grading-area
+cp TestListExamples.java grading-area
+
+cd grading-area
+
+javac -cp $CPATH *.java
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > test-result.txt
+
+result=`grep "OK" test-result.txt`
+echo $result
+
+if [[ $result == 'OK (1 test)' ]]
+then
+    echo 'You get a Pass'
+    exit
+fi
 
 
 # Draw a picture/take notes on the directory structure that's set up after
